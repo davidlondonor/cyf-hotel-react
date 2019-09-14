fe6d0a1385f958d296c10d5f062e3c3422b94a8a
davidlucky@gmail.com <davidlucky@gmail.com>
2019-09-14 14:02:19 -0500

    Hotel React

---
+++ src/Heading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel Nuevo</header>
+      <div className="imagenContenedor">
+        <img className="Logo-header" src="https://i.ibb.co/mGhDP7q/busy.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
